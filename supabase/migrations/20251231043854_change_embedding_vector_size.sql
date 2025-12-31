17484c56840bfdab0c956d429c6a38597e406b8a
srleom <shengrui.leom@gmail.com>
2025-12-31 15:00:28 +0800

    fix: change back to 1536 vector column size in embeddings table

--- supabase/migrations/20251231043854_change_embedding_vector_size.sql
+++
@@ -1,9 +0,0 @@
-alter table "public"."messages" drop constraint "messages_role_check";
-
-alter table "public"."embeddings" alter column "vector" set data type public.vector(768) using "vector"::public.vector(768);
-
-alter table "public"."messages" add constraint "messages_role_check" CHECK (((role)::text = ANY ((ARRAY['user'::character varying, 'assistant'::character varying])::text[]))) not valid;
-
-alter table "public"."messages" validate constraint "messages_role_check";
-
-
